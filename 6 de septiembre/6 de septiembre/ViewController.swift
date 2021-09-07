61649691082409274889b9747c2c83a7c1b2a9d0
Rubén Borbolla <53828155+rubenbc7@users.noreply.github.com>
2021-09-06 17:26:56 -0700

    Add files via upload

---
+++ 6 de septiembre/6 de septiembre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  6 de septiembre
+//
+//  Created by Alumno on 06/09/21.
+//  Copyright © 2021 Sebastian Encinas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
